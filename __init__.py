95ff09744778857a0585b4ea9e53c78c0b8f6b6f
yedp123 <71874235+yedp123@users.noreply.github.com>
2026-01-22 13:10:42 +0800

    Update __init__.py
    Registered a new CinematicLoaderNode. It takes a hidden string input (the filename) and outputs IMAGE

--- __init__.py
+++ __init__.py
@@ -1,17 +1,17 @@
-print("--- CINEMATIC NODE: INITIALIZING ---")
+print("--- CINEMATIC SUITE: INITIALIZING ---")
 
-from .cinematic_node import CinematicPromptNode
+from .cinematic_node import CinematicPromptNode, CinematicLoaderNode
 
 NODE_CLASS_MAPPINGS = {
-    "CinematicPromptNode": CinematicPromptNode
+    "CinematicPromptNode": CinematicPromptNode,
+    "CinematicLoaderNode": CinematicLoaderNode
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "CinematicPromptNode": "Cinematic Prompt Builder (Yedp)"
+    "CinematicPromptNode": "Cinematic Prompt Builder (Yedp)",
+    "CinematicLoaderNode": "Cinematic Reference Loader (Yedp)"
 }
 
-# This line tells ComfyUI where to find the JS file.
-# It MUST point to the "web" folder inside your node folder.
 WEB_DIRECTORY = "./web"
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
